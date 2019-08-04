824e4b96a441e0b2a4cffa3bc87c5965b0b3c22f
Matt <dohrann@gmail.com>
2019-08-04 22:19:50 +0000

    added author tracking for campgrounds and required login to create

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,11 +21,14 @@ router.get("/new", isLoggedIn, (req,res) => {
 });
 
 //CREATE - add new Campground to database
-router.post("/", (req,res) => {
+router.post("/", isLoggedIn, (req,res) => {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var author = req.body.author;
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	};
 	var newCampground = {name: name, image: image, description: desc, author: author};
 	Campground.create(newCampground, (err, newCg) => {
 		if(err){
