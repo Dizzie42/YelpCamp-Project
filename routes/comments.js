956a84bf1dcbeaaae002cb2c94d43c93570162c7
Matt <dohrann@gmail.com>
2019-08-05 20:04:22 +0000

    added destroy route for comments, also a delete button on comments

--- routes/comments.js
+++ routes/comments.js
@@ -61,6 +61,17 @@ router.put("/:comment_id", (req,res) => {
 	});
 });
 
+//Comments DESTROY route
+router.delete("/:comment_id", (req,res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/"+req.params.id);
+		}
+	});
+});
+
 //==================
 //Middleware
 //==================
